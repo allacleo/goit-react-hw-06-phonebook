4b99ca0ffdc0f787519265f5af12f0192790497e
Alla Sariieva <alla.cleo2gmail.com>
2020-01-12 20:44:49 +0200

    Updates

---
+++ precache-manifest.db576573d3bdace0777cb8f62267a690.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "affea065af05aa241474a2270e888037",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "15552827a5ae3f28de30",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "83765b152fe7733587fc",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.9730548a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.9730548a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "15552827a5ae3f28de30",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.9aa67f0b.chunk.js"
+  },
+  {
+    "revision": "8ef94a824e6f3ee6d7e7",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.b34c3905.js"
+  }
+]);
